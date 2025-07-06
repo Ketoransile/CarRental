8a6f58396d501ddb5a014b8dcd6e3f980f72beba
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-07-07 00:47:42 +0300

    edits vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+console.log("🧪 VITE_API_URL =", process.env.VITE_API_URL);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
