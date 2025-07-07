2a513b59d11c7200c64e39dc4acb14f4d67a2c72
Ketoran Sileshi <abdisileshi123@gmail.com>
2025-07-07 09:00:26 +0300

    updates vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-console.log("🧪 VITE_API_URL =", process.env.VITE_API_URL);
+// console.log("🧪 VITE_API_URL =", process.env.VITE_API_URL);
 
 // https://vite.dev/config/
 export default defineConfig({
